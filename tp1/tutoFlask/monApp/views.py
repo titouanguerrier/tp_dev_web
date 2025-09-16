eaa8e5ecba10adfad94484d31700188801a48830
Guerrier Titouan <o22401366@or-iut-i207-f02.iut45.univ-orleans.fr>
2025-09-16 16:52:17 +0200

    ajout de index.html about et contact et du render template et du css

--- tp1/tutoFlask/monApp/views.py
+++ tp1/tutoFlask/monApp/views.py
@@ -1,15 +1,17 @@
 from .app import app
+from flask import render_template
 
 @app.route('/')
-def index( ) :
-    return "Hello world !"
-@app.route('/about')
+@app.route('/index/')
+def index():
+    return render_template("index.html",title ="R3.01 Dev Web avec Flask",name="Cricri")
+
+@app.route('/about/')
 def about():
-    return app.config['ABOUT']
-if __name__== "__main__" :
-    app.run( )
-@app.route('/contact')
+    return render_template("about.html",title ="about",name=app.config['ABOUT'])
+
+@app.route('/contact/')
 def contact():
-    return app.config['CONTACT']
+    return render_template("contact.html",title ="contact ",name=app.config['CONTACT'])
 if __name__== "__main__" :
     app.run( )
